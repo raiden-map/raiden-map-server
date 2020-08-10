d8fd2f7d2f785fc3b3f3478f3f7165a7db9affb7
palace22 <palazzolo1995@gmail.com>
2020-08-10 11:48:09 +0200

    updated mongo credential

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TokenNetworkModule } from './token-network/token-network.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { environments } from './environments';
 
 //const mongodb = 'mongodb://localhost:27017/raiden-map'
 const mongodb = 'mongodb://51.136.5.3:27017/raiden-map'
 
 @Module({
   imports: [
-    MongooseModule.forRoot(mongodb, { useFindAndModify: false}),
+    MongooseModule.forRoot(mongodb, {
+      user: environments.mongoUser, pass: environments.mongoPsw,
+    }),
     TokenNetworkModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
